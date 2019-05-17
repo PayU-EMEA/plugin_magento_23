313da7153a27f53b6833782aab3a36f32f0f5806
Tomasz Regdos <tomasz.regdos@payu.pl>
2019-05-17 11:16:41 +0200

    Version update

--- Model/Config.php
+++ Model/Config.php
@@ -25,7 +25,7 @@ class Config implements PayUConfigInterface
     /**
      * Current Plugin Version
      */
-    const PLUGIN_VERSION = '1.0.2';
+    const PLUGIN_VERSION = '1.0.3';
 
     /**
      * @var \OpenPayU_Configuration
